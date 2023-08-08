fafa3b86df93762e0415714bc21502b78b0d1d05
ILikeVoid <kabidulla200@mail.ru>
2023-08-08 19:05:37 +0600

    refresh catalog-page

--- src/pages/CatalogPage/CatalogPage.tsx
+++ src/pages/CatalogPage/CatalogPage.tsx
@@ -1,34 +1,31 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import s from './CatalogPage.module.scss'
-import { Items } from '../../assets/data/categoriesData'
 import { NavLink } from 'react-router-dom'
+import { Items as Categories } from '../../assets/data/categoriesData'
 
 const CatalogPage = () => {
-	const [currentPage, setCurrentPage] = useState(1);
 
 	return (
 		<div className={s.catalog}>
 			<div className='container mx-auto'>
-				<h1>Каталог товаров</h1>
+				<h1>Каталог</h1>
 				<div className={s.catalog_items}>
-					<div>
+					<div className={s.catalog_nav}>
 						<nav>
-							{Items.map(item => (
-								<NavLink to='#' key={item.id}>
-									{item.name}
+							{Categories.map(category => (
+								<NavLink key={category.id}
+												 className={({ isActive, isPending }) => isPending ? s.pending : isActive ? s.active : ''}
+												 to={`category/${category.id}`}>
+									{category.name}
 								</NavLink>
 							))}
 						</nav>
 					</div>
-					<div className={s.catalog_list}>
-						{Items.map(item => (
-							<div className={s.item} key={item.id}>
-								<div className={s.img_block}>
-									<img src={item.img} alt='' />
-								</div>
-								<p>{item.name}</p>
-							</div>
-						))}
+					<div className={s.categories}>
+						{Categories.map(item => <NavLink to={`category/${item.id}`} key={item.id} className={s.category}>
+							<div className={s.img_block}><img src={item.img} alt='' /></div>
+							<span>{item.name}</span>
+						</NavLink>)}
 					</div>
 				</div>
 			</div>
