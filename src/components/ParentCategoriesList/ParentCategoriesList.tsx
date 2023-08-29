c8b9561f56b2694168416e3a7d7f46b2bf43e58a
ILikeVoid <kabidulla200@mail.ru>
2023-08-29 12:25:28 +0600

    create parent list in HomePage

---
+++ src/components/ParentCategoriesList/ParentCategoriesList.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import s from './ParentCategoriesList.module.scss'
+import { useGetParentQuery } from '../../redux/sevices/features/categoriesApi/categoriesApi'
+import { NavLink } from 'react-router-dom'
+
+const ParentCategoriesList = () => {
+
+	const { data: parents = [] } = useGetParentQuery()
+
+	return (
+		<div className={s.parent_list}>
+			<div className='container mx-auto'>
+				<div className={s.parent_list_items}>
+					{parents.map(parent => <NavLink to={`parent/${parent.id}`} className={s.parent}>
+						<div className={s.img_block}><img src={`http://192.168.100.24/${parent.image}`} alt='' /></div>
+						<div className={s.items}><p>{parent.title}</p></div>
+					</NavLink>)}
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default ParentCategoriesList
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import s from './HomePage.module.scss'
 import BrandsCarousel from '../../components/BrandsCarousel/BrandsCarousel'
 import Title from '../../components/Title/Title'
+import ParentCategoriesList from '../../components/ParentCategoriesList/ParentCategoriesList'
 
 const HomePage = () => {
 	return (
 		<div className={s.home}>
 			<Title />
 			<BrandsCarousel />
+			<ParentCategoriesList/>
 		</div>
 	)
 }
